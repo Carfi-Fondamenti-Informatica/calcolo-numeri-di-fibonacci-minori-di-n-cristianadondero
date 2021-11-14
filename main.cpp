947a8115ded37bb760ae50e31833241d6f8f19dd
cristianadondero <91587462+cristianadondero@users.noreply.github.com>
2021-11-14 15:23:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0, x=1, y=1, z=0;
+    cin>>n;
+    while(x<=n) {
+        x=y+z;
+        z=y;
+        y=x;
+        if(x<=n) {
+            cout<<x<<endl;
+        }
+
+    }
+    return 0;
 }
